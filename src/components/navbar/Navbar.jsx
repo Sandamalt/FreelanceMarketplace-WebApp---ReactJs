84e93c008f2975120b9f753f91a2ae50d2c187a0
sandamalt <sandamal.live@gmail.com>
2023-04-06 02:38:54 +0530

    Top Nav Bar Completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Navbar.scss"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const [active,setActive] = useState(false)
+    const [open,setOpen] = useState(false)
+
+
+    const isActive = ()=>{
+        window.scrollY > 0 ? setActive(true) : setActive(false)
+    }
+
+    useEffect(()=>{
+         window.addEventListener("scroll",isActive);
+
+         return () =>{
+            window.removeEventListener("scroll", isActive);
+         };
+    },[]);
+
+    const currentUser = {
+        id:1,
+        username: "Sandamal",
+        isSeller:true
+    }
+
   return (
-    <div className='navbar'>
+    <div className={active ? "navbar active": "navbar"}>
         <div className="container">
             <div className="logo">
+                {/* <Link to="/"> */}
                     <span className='text'> Fiverr</span>
+                {/* </Link> */}
                     <span className='dot'> .</span>
 
                 </div>
@@ -15,10 +41,41 @@ const Navbar = () => {
                     <span> Explore</span>
                     <span> English</span>
                     <span> Sign in</span>
-                    <span>Become a Seller </span>
-                    <button>Join</button>
+                    {!currentUser?.isSeller && <span>Become a Seller </span>}
+                    {!currentUser && <button>Join</button>}
+                    {currentUser && (
+                        <div className="user" onClick={()=>setOpen(!open)}>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3-3zj8G9gN1nMK6CygVjfsQN3auL1oeaE2g&usqp=CAU" alt="" />
+                            <span>{currentUser?.username}</span>
+                            {open && <div className="options">
+                                {
+                                    currentUser?.isSeller && (
+                                        <>
+                                        <span>Gigs</span>
+                                        <span>Add New Gig</span>
+
+                                        </>
+                                    )
+                                }
+                                <span>Orders</span>
+                                <span>Messages</span>
+                                <span>Logout</span>
+                            </div>}
+                        </div>
+                    )} 
             </div>
         </div>
+         {active  && (
+          <>
+
+         <hr />  
+          <div className="menu">
+            <span> Test</span>
+            <span> Test2</span>
+          </div>
+
+          </>
+          )}
      </div>
   )
 }
